6ac812f45cc29f5189355b10639829c2847de87c
bryanpintado <bryanp.0311@gmail.com>
2025-01-24 15:40:23 -0500

    fixed bug in notebook that messed with indexes modified notebook to output fully processed and formatted csv file
    need to add ui to app and selection

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv("league_info.csv")
+df = pd.read_csv("la_liga.csv")
 
 
+home_games_df = df[df['home_team'].str.contains('FC Barcelona | barcelona', case= False, na=False)].reset_index(drop=False)
+away_games_df = df[df['away_team'].str.contains('FC barcelona | barcelona',case=False,na=False)]
 
-st.title('FC Barcelona Stats')
-st.write(df)  
+
+#STREAMLIT APP
+st.title('FC Barcelona Dash Board')
+st.divider()
+
+
+st.header('Home Games')
+st.dataframe(home_games_df)  
